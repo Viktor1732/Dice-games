b9b393f44b63f9b3b8f44da92c529ab71c3c507c
Viktor1732 <programmer.kras@gmail.com>
2022-04-23 18:44:23 +0700

    Отредактировано окно. Добавлены 2 метки для кубиков. Изменен размер иконки.

--- dice_game.py
+++ dice_game.py
@@ -2,4 +2,14 @@ from tkinter import *
 
 
 root = Tk()
+root.title('Dice game! Make a throw!')
+root.geometry('400x200')
+root.iconphoto(True, PhotoImage(file=('iconka.png')))
+root.resizable(width=False, height=False)
+holst = PhotoImage(file=('holst.png'))
+Label(root, image=holst).pack()
+lab1 = Label(root)
+lab2 = Label(root)
+lab1.place(relx=0.3, rely=0.5, anchor=CENTER)
+lab2.place(relx=0.7, rely=0.5, anchor=CENTER)
 root.mainloop()
